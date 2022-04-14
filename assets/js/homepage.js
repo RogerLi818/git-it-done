c5da31d90188ee0df19a32ee2c615929a0fd0076
RogerLi818 <jackliteng@yahoo.com>
2022-04-14 08:53:11 -0700

    6.2.4

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,6 +9,12 @@ var getUserRepos = function(user){
     });
   });
 }
+getUserRepos("rogerli818");
 
-getUserRepos("facebook");
+var userFormEl=document.querySelector("#user-form");
+var nameInputEl=document.querySelector("#username");
 
+var formSubmitHandler=function(event){
+  event.preventDefault();
+  console.log(event);
+}
