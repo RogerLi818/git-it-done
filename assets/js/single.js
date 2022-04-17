b6ce7445286bbb24fb61609128ac8272edabfe86
RogerLi818 <jackliteng@yahoo.com>
2022-04-17 11:55:39 -0700

    finish repo single page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,25 @@
 var issueContainerEl=document.querySelector("#issues-container");
 var limitWarningEl=document.querySelector("#limit-warning");
+var repoNameEl=document.querySelector("#repo-name")
+
+getRepoName=function(){
+    //grab repo name from url query string
+    var queryString=document.location.search;
+    var repoName=queryString.split("=")[1];
+    
+    if(repoName){
+        //display repo name on the page
+        repoNameEl.textContent=repoName;
+        getRepoIssues(repoName);
+    }else{
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
+};
 
 getRepoIssues=function(repo){
+   
     var apiUrl="http://api.github.com/repos/"+ repo +"/issues?direction=asc";
 
     fetch(apiUrl).then(function(response){
@@ -18,13 +36,11 @@ getRepoIssues=function(repo){
                 }
             });
         }else{
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     })
-    console.log(repo);
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues=function(issues){
 
@@ -75,3 +91,5 @@ var displayWarning=function(repo){
     //append to warning container
     limitWarningEl.appendChild(linkEl);
 }
+
+getRepoName();
